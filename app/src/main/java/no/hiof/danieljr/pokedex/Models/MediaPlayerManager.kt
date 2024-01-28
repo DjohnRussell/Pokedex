19a3d6b2bd72a751fc35d76298da43640b826732
danieljohnrussell <danieljr@hiof.no>
2024-01-28 20:21:23 +0100

    added pokemon soundtrack

---
+++ app/src/main/java/no/hiof/danieljr/pokedex/Models/MediaPlayerManager.kt
@@ -0,0 +1,37 @@
+package no.hiof.danieljr.pokedex.Models
+
+import androidx.compose.runtime.Composable
+
+
+
+import android.content.Context
+import android.media.MediaPlayer
+import no.hiof.danieljr.pokedex.R
+
+
+object MediaPlayerManager {
+    private var mediaPlayer: MediaPlayer? = null
+    private var isMuted = false
+
+    fun initialize(context: Context, audio : Int) {
+        mediaPlayer = MediaPlayer.create(context, audio)
+        mediaPlayer?.isLooping = true
+    }
+
+    fun start() {
+        mediaPlayer?.start()
+    }
+
+    fun pause() {
+        mediaPlayer?.pause()
+    }
+
+    fun toggleMute() {
+        isMuted = !isMuted
+        mediaPlayer?.setVolume(if (isMuted) 0f else 1f, if (isMuted) 0f else 1f)
+    }
+
+    fun isMuted(): Boolean {
+        return isMuted
+    }
+}
--- app/src/main/java/no/hiof/danieljr/pokedex/ui/Screens/welcomeScreen.kt
+++ app/src/main/java/no/hiof/danieljr/pokedex/ui/Screens/welcomeScreen.kt
@@ -1,5 +1,6 @@
 package no.hiof.danieljr.pokedex.ui.theme.Screens
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,12 +10,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import no.hiof.danieljr.pokedex.Models.MediaPlayerManager
 import no.hiof.danieljr.pokedex.R
 
 
 @Composable
 fun welcome(painer: Painter, click: () -> Unit){
+    val context = LocalContext.current
+    MediaPlayerManager.initialize(context, R.raw.pokemon)
+    MediaPlayerManager.start()
 
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
