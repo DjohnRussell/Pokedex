c5d23da3378e6dd3cd7742b3691a4bd34726ed45
danieljohnrussell <danieljr@hiof.no>
2024-01-30 00:12:48 +0100

    Added Glide for pictuares loading
    still working on getting it right

--- app/src/main/java/no/hiof/danieljr/pokedex/ui/Screens/home.kt
+++ app/src/main/java/no/hiof/danieljr/pokedex/ui/Screens/home.kt
@@ -1,7 +1,16 @@
 package no.hiof.danieljr.pokedex.ui.Screens
 
 import androidx.compose.runtime.Composable
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
+
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun home() {
-}
+        //ToDo - fix the pics. and make logic to desplay diffrent pokemon based on seartches
+    GlideImage(model = "https://www.pngegg.com/en/png-cygki", contentDescription = "Blastoise pic")
+
+
+
+    }
